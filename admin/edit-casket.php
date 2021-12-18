623ba19bcb55149f800724500f20412943b38595
Junel <sacrojunel@gmail.com>
2021-12-18 09:22:05 +0800

    add image upload

--- admin/edit-casket.php
+++ admin/edit-casket.php
@@ -5,15 +5,27 @@
 		$id = $_POST['casket_id'];
 		$casket = $_POST['casket'];
         $amount = $_POST['amount'];
-		
-		$sql = "UPDATE casket SET casket_type = '$casket', amount = '$amount' WHERE casket_id = '$id'";
-		if($conn->query($sql)){
-			$_SESSION['edit-casket-success'] = 'Casket update successfull';
-		}
-		else{
-			$_SESSION['edit-casket-error'] = $conn->error;
-		}
+		$service_type = $_POST['package-type'];
 
+		
+        $filename = $_FILES['file']['name'];
+        $target_dir = "casketimage/";
+        $target_file = $target_dir. basename($filename);
+      
+
+			if(move_uploaded_file($_FILES['file']['tmp_name'], $target_file)){
+
+				$sql = "UPDATE casket 
+						SET casket_type = '$casket', amount = '$amount', image ='$filename',
+						service_id= (SELECT service_id FROM service WHERE service_id='$service_type')
+						WHERE casket_id = '$id'";
+				if($conn->query($sql)){
+					$_SESSION['edit-casket-success'] = 'Casket update successfull';
+				}
+				else{
+					$_SESSION['edit-casket-error'] = $conn->error;
+				}
+		  	}
 	}
 	else{
 		$_SESSION['edit-casket-error'] = 'Select casket to edit first';

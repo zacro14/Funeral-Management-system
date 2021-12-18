31519554230d546a717d849d66633ef20da90383
Junel <sacrojunel@gmail.com>
2021-12-18 09:23:48 +0800

    ok

--- admin/fetch-casket.php
+++ admin/fetch-casket.php
@@ -5,7 +5,10 @@
     {
         $id = $_POST['id'];
 
-        $fetch_casket = "SELECT * FROM casket LEFT JOIN casket_qty ON casket.casket_id = casket_qty.casket_id LEFT JOIN service ON service.service_id = casket.service_id WHERE casket.casket_id = '".$id."'";
+        $fetch_casket = "SELECT * FROM casket 
+        LEFT JOIN casket_qty ON casket.casket_id = casket_qty.casket_id 
+        LEFT JOIN service ON service.service_id = casket.service_id 
+        WHERE casket.casket_id = '".$id."'";
         $query = $conn->query($fetch_casket);
         $row_casket = $query->fetch_assoc();
 

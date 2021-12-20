6c112564e86d5086594ca7fd3f3da6d3a4d90d67
Junel <sacrojunel@gmail.com>
2021-12-20 17:17:57 +0800

    add where clause

--- admin/fetch-payment.php
+++ admin/fetch-payment.php
@@ -8,7 +8,8 @@
         $fetch_payment ="SELECT * FROM payments 
                         INNER JOIN contract ON contract.contract_unique_id = payments.contract_id 
                         INNER JOIN client ON client.client_id = payments.client_id  
-                        INNER JOIN branches ON branches.branch_id = payments.branch_id";
+                        INNER JOIN branches ON branches.branch_id = payments.branch_id
+                        WHERE payment_id = $id ";
 
         $query = $conn->query($fetch_payment);
         $row_payment = $query->fetch_assoc();

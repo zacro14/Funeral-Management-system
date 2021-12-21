50a7447ff5f5ae147f7ac599e37c6c1f29203208
Junel <sacrojunel@gmail.com>
2021-12-21 22:41:52 +0800

    add alert for ediit

--- admin/includes/employee-alert-message.php
+++ admin/includes/employee-alert-message.php
@@ -18,5 +18,24 @@
         unset($_SESSION['add-employee-success']);
     }
 
- 
+    if(isset($_SESSION['edit-employee-success'])){
+        echo "
+        <div class='alert alert-success alert-dismissible'>
+            <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+            <i class='fa fa-check fa-lg'></i>".$_SESSION['edit-employee-success']."
+        </div>
+    ";
+    unset($_SESSION['edit-employee-success']);
+    }
+
+    if(isset($_SESSION['edit-employee-error'])){
+        echo "
+        <div class='alert alert-danger alert-dismissible'>
+            <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+            <i class='fa fa-exclamation fa-lg'></i>".$_SESSION['edit-employee-error']."
+        </div>
+    ";
+    unset($_SESSION['edit-employee-error']);
+    }
+
 ?>
